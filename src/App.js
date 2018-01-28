702f8145bd9592cfae790055146fec0b1a88c13e
Javier Pérez <surtich@gmail.com>
2018-01-29 00:35:39 +0100

    Refactor using destructuring

--- src/App.js
+++ src/App.js
@@ -50,10 +50,11 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <input type="text" onChange={this.onSearchChange} />
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
