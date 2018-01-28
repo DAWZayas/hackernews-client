722224c41ea8dd713b3afc21d1ba1fb9feb39739
Javier Pérez <surtich@gmail.com>
2018-01-29 00:33:39 +0100

    Added search input as example of uncontrolled input

--- src/App.js
+++ src/App.js
@@ -20,27 +20,40 @@ const list = [
   }
 ];
 
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list
+      list: list,
+      searchTerm: ""
     };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
   onDismiss(id) {
-    return () => {
-      const isNotId = item => item.objectID !== id;
-      const updatedList = this.state.list.filter(isNotId);
-      this.setState({ list: updatedList });
-    }
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => (
+        <input type="text" onChange={this.onSearchChange} />
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -49,15 +62,12 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={this.onDismiss(item.objectID)}
-                type="button"
-              >
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">
                 Dismiss
               </button>
             </span>
           </div>
-        ))}
+        )}
       </div>
     );
   }
